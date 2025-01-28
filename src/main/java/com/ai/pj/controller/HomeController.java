ab4764b8ac1d46b1d6ab13e34139d33cb08b1f02
KCR <tbvj61@gmail.com>
2025-01-28 16:33:05 +0900

    사이드바 접기, 헤더 생성

--- src/main/java/com/ai/pj/controller/HomeController.java
+++ src/main/java/com/ai/pj/controller/HomeController.java
@@ -2,16 +2,20 @@ package com.ai.pj.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
 
     @GetMapping("/")
-    public String home(){
-        return "index"; // index.mustache 또는 index.html 렌더링
+    public String home(Model model){
+        model.addAttribute("managerName", "Admin User"); // 실제 로그인 사용자 정보를 여기에 넣을 수도 있음
+        return "home"; // home.mustache 렌더링
     }
 
     // /home 요청을 /로 리다이렉트
@@ -19,5 +23,6 @@ public class HomeController {
     public String redirectToHome() {
         return "redirect:/"; // /home 요청을 기본 경로로 리다이렉트
     }
+
 
 }
